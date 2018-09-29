bbc2979934ea863176abede707c34d3f7d1ae052
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-09-29 18:10:36 +0100

    image switching working!!!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,44 @@
 @extends('master')
 
 @section('content')
-<div class="row">
-
-    @foreach($images as $image)
-        <div class="card col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2">
-            <img id="{{ $image->hash }}" class="card-img-top img-fluid" src="{{ $image->URLRaw() }}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">ID: {{ $image->id }}</h5>
-                <button id="{{ $image->hash }}_button" type="button" class="btn btn-primary">Annotate</button>
-                <p class="card-text">{{ $content }}</p>
-                <p class="card-text">
-                    <small class="text-muted">{{ $image->created_at->diffForHumans() }}</small>
-                </p>
-            </div>
-        </div>
+    <div class="row">
 
         <script>
-            image = document.getElementById('{{ $image->hash }}');
-            switchButton = document.getElementById(('{{ $image->hash }}_button'));
-
-            switchButton.onclick = function (e) {
-                if (image.src.valueOf() === "{{ $image->URLRaw() }}".valueOf()) {
-                    image.src = "{{ $image->URLPredictions() }}";
-                } else if (image.src.valueOf() === "{{ $image->URLPredictions() }}".valueOf()) {
-                    image.src = "{{ $image->URLRaw() }}";
+            function imageSwitcher(image, URLRaw, URLPredictions) {
+                if (image.src === URLRaw) {
+                    image.src = URLPredictions;
+                } else if (image.src === URLPredictions) {
+                    image.src = URLRaw;
                 } else {
-                    console.log('failed to set new src value')
+                    console.log('failed to set new src value for image')
                 }
-            };
+            }
 
         </script>
-    @endforeach
 
-</div>
+        @foreach($images as $image)
+            <div class="card col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2">
+                <img id="{{ $image->hash }}" class="card-img-top img-fluid" src="{{ $image->URLRaw() }}"
+                     alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">ID: {{ $image->id }}</h5>
+
+                    <button
+                        onclick="imageSwitcher(document.getElementById({{ $image->hash }}), '{{ $image->URLRaw() }}', '{{ $image->URLPredictions() }}')"
+                        type="button"
+                        class="btn btn-primary"
+                    >
+                        Annotate
+                    </button>
+
+                    <p class="card-text">{{ $content }}</p>
+                    <p class="card-text">
+                        <small class="text-muted">{{ $image->created_at->diffForHumans() }}</small>
+                    </p>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 
 @endsection
